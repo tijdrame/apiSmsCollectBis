e88c203c4d59d19c5356bb81bc227ef18ce6d9b2
Tidiane <tijdrame@gmail.com>
2021-08-13 12:21:34 +0100

    Initial version of apiSmsCollect generated by JHipster-7.0.1

---
+++ src/main/java/com/boa/api/config/ApplicationProperties.java
@@ -0,0 +1,12 @@
+package com.boa.api.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Api Sms Collect.
+ * <p>
+ * Properties are configured in the {@code application.yml} file.
+ * See {@link tech.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {}
